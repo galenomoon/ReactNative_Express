cd830fdd70669dda24f83bbae2349ccd053cf9ca
galenomoon <gui.galenocover@gmail.com>
2022-07-14 22:32:11 -0300

    dataPicker added

--- src/pages/AddTask/index.jsx
+++ src/pages/AddTask/index.jsx
@@ -1,26 +1,54 @@
 import { StyleSheet, Text, View, Modal, Alert, TouchableOpacity as TO, TextInput } from 'react-native'
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
+import moment from 'moment'
+import DateTimePicker from '@react-native-community/datetimepicker'
 
 const AddTask = ({ modalToggle, setModalToggle }) => {
   const [newDescription, setNewDescription] = useState('')
+  const [date, setDate] = useState(new Date())
+  const [toggleDatePicker, setToggleDatePicker] = useState(true)
 
   const save = (description) => Alert.alert(description.description)
 
-  const cancel = () => { }
+  const cancel = () => setModalToggle(!modalToggle)
 
   const buttons = [{ label: 'Save', fn: save }, { label: 'Cancel', fn: cancel }]
-  const onCloseModal = () => {
-    return setModalToggle(false)
+
+  const onCloseModal = () => setNewDescription('')
+
+  const getDatePicker = () => {
+
+    const dateString = () => moment(date).format('ddd, D [de] MMMM [de] YYYY')
+
+    return (
+      <View style={{ alignItems: 'center', marginBottom: 10 }}>
+        <TO onPress={() => setToggleDatePicker(true)}>
+          <Text style={styles.date}>
+            {dateString()}
+          </Text>
+        </TO>
+      </View>)
   }
 
+  const datePicker = () => toggleDatePicker &&
+    <DateTimePicker
+      value={date}
+      onChange={(_, date) => [setDate(date), setToggleDatePicker(false)]}
+      mode='date'
+    />
+
+  useEffect(() => {
+    onCloseModal()
+  }, [modalToggle])
+
   return (
-    <Modal transparent visible={true} onRequestClose={() => onCloseModal()} animationType="slide">
+    <Modal transparent visible={modalToggle} animationType="slide">
       <View style={styles.overlay}>
         <View style={styles.modal}>
           <View style={styles.modal.header}>
             <Text style={styles.modal.title}>New Task</Text>
-            <TO onPress={() => onCloseModal()}>
+            <TO onPress={() => cancel()}>
               <Icon name='close' size={30} color='#FFF' style={{ backgroundColor: 'rgba(0,0,0,0.3)', borderRadius: 10, padding: 1 }} />
             </TO>
           </View>
@@ -32,6 +60,8 @@ const AddTask = ({ modalToggle, setModalToggle }) => {
               onChangeText={(text) => setNewDescription(text)}
             />
           </View>
+          {datePicker()}
+          {getDatePicker()}
           <View style={styles.modal.footer}>
             <View style={styles.modal.footer.box}>
               {buttons.map((btn, idx) => <TO onPress={() => btn.fn({ description: newDescription })} key={idx}><Text style={styles.modal.redText}>{btn.label}</Text></TO>)}
@@ -101,5 +131,8 @@ const styles = StyleSheet.create({
     borderColor: '#aF033d',
     width: '90%',
     height: 60,
+  },
+  date: {
+    fontSize: 25
   }
 })
--- src/pages/TaskList/index.jsx
+++ src/pages/TaskList/index.jsx
@@ -1,14 +1,20 @@
-import { StyleSheet, FlatList, View } from 'react-native'
+import { StyleSheet, FlatList, View, TouchableOpacity as TO } from 'react-native'
 import React, { useState } from 'react'
 
+//components
 import Header from '../../components/Header'
 import Task from '../../components/Task'
 
+//pages
 import AddTask from '../AddTask'
+
+// styles
+import IoniconsIcon from 'react-native-vector-icons/Ionicons'
+
 
 const TaskList = () => {
   const [isShowingCheckedTasks, setIsShowingCheckedTasks] = useState(true)
-  const [modalToggle, setModalToggle] = useState(true)
+  const [modalToggle, setModalToggle] = useState(false)
   const tasks = [
     { id: 1, description: 'Compor', estimateAt: new Date() },
     { id: 2, description: 'Cantar', estimateAt: new Date() },
@@ -33,10 +39,27 @@ const TaskList = () => {
           }
         />
       </View>
+      <TO onPress={() => setModalToggle(!modalToggle)} style={styles.btn}>
+        <IoniconsIcon name='add' size={40} color='#fff' />
+      </TO>
     </>
   )
 }
 
 export default TaskList
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  btn: {
+    flex: 1,
+    backgroundColor: "#a8033d",
+    position: 'absolute',
+    right: 1,
+    bottom: 1,
+    margin: 20,
+    borderRadius: 100,
+    width: 50,
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+})
